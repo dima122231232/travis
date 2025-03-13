6cc2363e8d73848080b837bf97ceb37743ca2ef5
dima122231232 <115538623+dima122231232@users.noreply.github.com>
2025-03-13 22:33:50 +0300

    Add files via upload

--- js/albom-audio.js
+++ js/albom-audio.js
@@ -9,14 +9,14 @@ document.addEventListener("DOMContentLoaded", () => {
     const durationEl = document.getElementById("duration");
 
     const tracks = [
-        '/audio/Utopia.mp3',
-        '/audio/HIGHEST IN THE ROOM.mp3',
-        '/audio/Travis Scott - WAKE UP.mp3',
-        '/audio/Huncho Jack.mp3',
-        '/audio/goosebumps (feat. Kendrick Lamar).mp3',
-        '/audio/Mamacita.mp3',
-        '/audio/The Prayer.mp3',
-        '/audio/Meadow Creek.mp3',
+        'audio/Utopia.mp3',
+        'audio/HIGHEST IN THE ROOM.mp3',
+        'audio/Travis Scott - WAKE UP.mp3',
+        'audio/Huncho Jack.mp3',
+        'audio/goosebumps (feat. Kendrick Lamar).mp3',
+        'audio/Mamacita.mp3',
+        'audio/The Prayer.mp3',
+        'audio/Meadow Creek.mp3',
     ];
 
     let currentTrackIndex = 0;
